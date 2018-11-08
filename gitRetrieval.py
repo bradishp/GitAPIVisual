abc8a41587fae86eccd35be141c79e7fbe75bc9e
Philip Bradish <bradishp@tcd.ie>
2018-11-08 18:29:26 +0000

    Wrote a function that uses github API to print out info on a user. Also wrote a test that calls it.

--- gitRetrieval.py
+++ gitRetrieval.py
@@ -1,9 +1,24 @@
 from github import Github
+import sys
+def get_info_on_developer(name=None, password=None):
+    if name is None or password is None:
+        g = Github("bbbbce0775c88233af65f275dadf6662e5531562") # Default Github instance using my token
+    else:
+        g= Github(name, password)
 
-# Create Github instance
-g = Github("XanthusXX", "password")
+    print g.get_user().login + "'s repositories:"
 
-# Test get repositories:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+    collaborators = set([])
+    for repo in g.get_user().get_repos():
+        print repo.name + ", ",
+        for contributor in repo.get_contributors():
+            if contributor.login != g.get_user().login:
+                collaborators.add(contributor)
 
+    print "\nThe public repositories of people who have worked with " + g.get_user().login + ":"
+    for person in collaborators:
+        print "\n" + person.login + ":",   
+        for repo in person.get_repos():
+            print repo.name + ", ",  
+    sys.stdout.flush()
+    return True
--- testGitRetrieval.py
+++ testGitRetrieval.py
@@ -5,8 +5,8 @@ class TestGitRetrieval(unittest.TestCase):
 
     # Tests that github is being accessed and data is being retrieved
     def test_git_retrieval(self):
-        # Placeholder test
-        self.assertEqual(True, True)
+        # Make sure Github can be accessed.
+        self.assertEqual(get_info_on_developer(), True) 
 
 if __name__ == '__main__':
     unittest.main()
