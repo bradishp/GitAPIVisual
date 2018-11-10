3932de2e11a9951b4db234e1897358fb64a95468
Philip Bradish <bradishp@tcd.ie>
2018-11-10 15:56:00 +0000

    Cleaned up code. Mae a class for GitUserInfo.

--- gitRetrieval.py
+++ gitRetrieval.py
@@ -1,24 +1,37 @@
-from github import Github
 import sys
-def get_info_on_developer(name=None, password=None):
-    if name is None or password is None:
-        g = Github("bbbbce0775c88233af65f275dadf6662e5531562") # Default Github instance using my token
-    else:
-        g= Github(name, password)
+from github import Github
 
-    print g.get_user().login + "'s repositories:"
+class GitUserInfo:
 
-    collaborators = set([])
-    for repo in g.get_user().get_repos():
-        print repo.name + ", ",
-        for contributor in repo.get_contributors():
-            if contributor.login != g.get_user().login:
-                collaborators.add(contributor)
+    def __init__(self, user_identifier, password=None):
+        if password is None:
+            self.github_instance = Github(user_identifier)
+        else:
+            self.github_instance = Github(user_identifier, password)
 
-    print "\nThe public repositories of people who have worked with " + g.get_user().login + ":"
-    for person in collaborators:
-        print "\n" + person.login + ":",   
-        for repo in person.get_repos():
-            print repo.name + ", ",  
-    sys.stdout.flush()
-    return True
+    def display_developer_repos(self):
+        repo_list = self.github_instance.get_user().login + "'s repositories: "
+
+        for repo in self.github_instance.get_user().get_repos():
+            repo_list += repo.name + ", "
+        print repo_list[:-2]
+        sys.stdout.flush()
+        return True
+
+    def gather_info_on_collaborators(self):
+        collaborators_repos = "The public repositories of people who have worked with " + self.github_instance.get_user().login + ": \n"
+        collaborators = set([])
+        for repo in self.github_instance.get_user().get_repos():
+            for contributor in repo.get_contributors():
+                if contributor.login != self.github_instance.get_user().login:
+                    collaborators.add(contributor)
+        for person in collaborators:
+            collaborators_repos += person.login + ": "  
+            for repo in person.get_repos():
+                collaborators_repos += repo.name +  ", "
+            collaborators_repos += "\n"
+        print collaborators_repos
+        sys.stdout.flush()
+        return True
+
+
--- testGitRetrieval.py
+++ testGitRetrieval.py
@@ -1,12 +1,14 @@
 import unittest
-from gitRetrieval import *
+from gitRetrieval import GitUserInfo
  
 class TestGitRetrieval(unittest.TestCase):
 
     # Tests that github is being accessed and data is being retrieved
     def test_git_retrieval(self):
         # Make sure Github can be accessed.
-        self.assertEqual(get_info_on_developer(), True) 
+        gitUser = GitUserInfo("bbbbce0775c88233af65f275dadf6662e5531562")
+        assert(gitUser.display_developer_repos(), True) 
+        assert(gitUser.gather_info_on_collaborators(), True)
 
 if __name__ == '__main__':
     unittest.main()
