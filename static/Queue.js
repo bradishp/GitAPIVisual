a76dfacf1d38aabdb0c18bef40da2d0590edc2ed
Philip Bradish <bradishp@tcd.ie>
2018-11-24 12:49:38 +0000

    Got the right code for Queue.js and increased the size of the pie chart.

--- static/Queue.js
+++ static/Queue.js
@@ -1,2 +1,81 @@
-//code.iamkate.com http://code.iamkate.com/javascript/queues/
-function Queue(){var a=[],b=0;this.getLength=function(){return a.length-b};this.isEmpty=function(){return 0==a.length};this.enqueue=function(b){a.push(b)};this.dequeue=function(){if(0!=a.length){var c=a[b];2*++b>=a.length&&(a=a.slice(b),b=0);return c}};this.peek=function(){return 0<a.length?a[b]:void 0}};
+//Code taken from the tutorial at http://adilmoujahid.com/posts/2015/01/interactive-data-visualization-d3-dc-python-mongodb/
+(function() {
+    var slice = [].slice;
+  
+    function queue(parallelism) {
+      var q,
+          tasks = [],
+          started = 0, // number of tasks that have been started (and perhaps finished)
+          active = 0, // number of tasks currently being executed (started but not finished)
+          remaining = 0, // number of tasks not yet finished
+          popping, // inside a synchronous task callback?
+          error = null,
+          await = noop,
+          all;
+  
+      if (!parallelism) parallelism = Infinity;
+  
+      function pop() {
+        while (popping = started < tasks.length && active < parallelism) {
+          var i = started++,
+              t = tasks[i],
+              a = slice.call(t, 1);
+          a.push(callback(i));
+          ++active;
+          t[0].apply(null, a);
+        }
+      }
+  
+      function callback(i) {
+        return function(e, r) {
+          --active;
+          if (error != null) return;
+          if (e != null) {
+            error = e; // ignore new tasks and squelch active callbacks
+            started = remaining = NaN; // stop queued tasks from starting
+            notify();
+          } else {
+            tasks[i] = r;
+            if (--remaining) popping || pop();
+            else notify();
+          }
+        };
+      }
+  
+      function notify() {
+        if (error != null) await(error);
+        else if (all) await(error, tasks);
+        else await.apply(null, [error].concat(tasks));
+      }
+  
+      return q = {
+        defer: function() {
+          if (!error) {
+            tasks.push(arguments);
+            ++remaining;
+            pop();
+          }
+          return q;
+        },
+        await: function(f) {
+          await = f;
+          all = false;
+          if (!remaining) notify();
+          return q;
+        },
+        awaitAll: function(f) {
+          await = f;
+          all = true;
+          if (!remaining) notify();
+          return q;
+        }
+      };
+    }
+  
+    function noop() {}
+  
+    queue.version = "1.0.7";
+    if (typeof define === "function" && define.amd) define(function() { return queue; });
+    else if (typeof module === "object" && module.exports) module.exports = queue;
+    else this.queue = queue;
+  })();
