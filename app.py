534433277159a58ca79a411825b81c60b6f13fc4
Philip Bradish <bradishp@tcd.ie>
2018-11-29 11:32:41 +0000

    Added breakdown of other section as another pie chart.

--- app.py
+++ app.py
@@ -3,14 +3,31 @@ from flask import Flask, render_template
 from gitRetrieval import generate_info, get_minor_languages
 
 username = ""
+major_languages = None
+minor_languages = None
 
 app = Flask(__name__)
+
+@app.route("/generate/info/other")
+def get_minor_language_info():
+    global minor_languages
+    if minor_languages:
+        return minor_languages
+    minor_languages = get_minor_languages()
+    return minor_languages
+
+@app.route("/other")
+def display_minor_language_info():
+    return render_template('visualOther.html', name=None)
 
 @app.route("/generate/info")
 def get_info():
     global username
-    languages_json = generate_info(username)
-    return languages_json
+    global major_languages
+    if major_languages:
+        return major_languages
+    major_languages = generate_info(username)
+    return major_languages
 
 @app.route("/")
 def display_info():
